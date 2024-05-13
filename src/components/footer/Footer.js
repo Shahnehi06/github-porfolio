d8d2d36f15758de31f400fbe734a6d26bd91b77d
Nehi Shah <160354672+Shahnehi06@users.noreply.github.com>
2024-05-13 23:07:40 +0530

    Add files via upload

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+import { Fade } from "react-reveal";
+import { greeting } from "../../portfolio.js";
+/* eslint-disable jsx-a11y/accessible-emoji */
+
+export default function Footer(props) {
+  return (
+    <div className="footer-div">
+      <Fade>
+        <p className="footer-text" style={{ color: props.theme.secondaryText }}>
+          Copyright <span role="img">©</span> 2024 {greeting.title2}
+        </p>
+      </Fade>
+    </div>
+  );
+}
